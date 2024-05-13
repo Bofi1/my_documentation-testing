9175d13f05eee6f1f8a5fe1dba3cc15ed493be29
bofi1 <angelborque03github@gmail.com>
2024-05-12 22:05:19 -0700

    Javascript seccion added & variables added

---
+++ JavaScript/variables.js
@@ -0,0 +1,8 @@
+// variables //
+
+let uno        // "let" tu puedes cambiar el valor y no hace falta INICIALIZAR
+const dos = 1   // "const" NO puedes cambiar el valor y AHUEVO la tienes que INICIALIZAR
+
+const num = 2;
+num = 6;
+console.log(num);
