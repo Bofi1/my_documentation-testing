46eb4c73e3ebc8d04faca36c2ffe903958428f7b
Bofi1 <150561585+Bofi1@users.noreply.github.com>
2024-05-12 22:06:07 -0700

    ejemplo eliminado

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -3,6 +3,3 @@
 let uno        // "let" tu puedes cambiar el valor y no hace falta INICIALIZAR
 const dos = 1   // "const" NO puedes cambiar el valor y AHUEVO la tienes que INICIALIZAR
 
-const num = 2;
-num = 6;
-console.log(num);
