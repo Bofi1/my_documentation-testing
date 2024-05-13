f6c5a82f56ba69cf4cac48e4408d5eb11d06999d
bofi1 <angelborque03github@gmail.com>
2024-05-12 22:24:50 -0700

    boolean seccion added

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -1,5 +1,36 @@
-// variables //
+// --- variables --- //
 
-let uno        // "let" tu puedes cambiar el valor y no hace falta INICIALIZAR
-const dos = 1   // "const" NO puedes cambiar el valor y AHUEVO la tienes que INICIALIZAR
+let uno;        // "let" tu puedes cambiar el valor y no hace falta INICIALIZAR
+const dos = 1;  // "const" NO puedes cambiar el valor y AHUEVO la tienes que INICIALIZAR
 
+
+
+
+// --- booleanos --- //
+
+let onSale = true;  // verdadero
+let onSalee = false; // falso
+
+// cualquier cosa lo puedes convertir a booleano usando lo siguiente 
+console.log(Boolean()) 
+
+// ej.
+console.log(Boolean(100)) // = true
+
+
+// truthy & falsy
+
+    // estos son todos lo valores falsy (false)
+
+        // false
+        // null
+        // undefined
+        // 0
+        // -0
+        // NaN
+        // An empty string ('')
+
+
+    // estos son todos lo valores truthy (true)
+
+        // todo lo que no estaaba en la lista falsy es truthy
