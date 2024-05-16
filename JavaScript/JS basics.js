5f148d95b39a44bde7582b47b3017f259659b146
bofi1 <angelborque03github@gmail.com>
2024-05-16 17:48:20 -0600

    arrays added

--- JavaScript/JS basics.js
+++ JavaScript/JS basics.js
@@ -269,7 +269,34 @@ function square(x) {
 
 
 
+// -------------------------- arrays -------------------------- //
 
+const strings = ["a", "b", "c"];      // ejemplo de array [0,1,2,3,4,5] la primer posicion es 0
+const numbers = [1, 2, 3];
+const mixed = ["a", 2, false];
+const empty = [];
+
+const arrays = [[1, 2, 3]];           // tambien puedes poner uno sobre otro
+
+arrays.length                         // muestra cuanto es el tamaño del array
+
+  // ejemplo de como usar length 
+  // (obtener el ultimo valor de un array)
+      const arrOne = [1, 2, 3, 4];  
+      const arrTwo = ['a', 'b', 3, 1, 'c'];
+
+      const getLast = function (arr) {
+         return arr[arr.length - 1];  // <----
+      }
+
+      console.log(getLast(arrOne), getLast(arrTwo));
+
+
+    // -----------
+    const arr = [1, 2, 3, 4];
+    const index = arr.indexOf(2); // 1
+    
+    console.log(arr[index] === 2); // arr[1] === 2 es igual a true
 
 
 
---
+++ prueba.js
@@ -0,0 +1,10 @@
+const arr = [
+    1, 
+    [
+        'hi', 
+        'bye',
+        [1, 2 , 3]
+    ]
+];
+ 
+console.log(arr[1][1][1]);
