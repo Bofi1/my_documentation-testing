a398f29fae648d05870f82c6f9155fd049c16c7a
bofi1 <angelborque03github@gmail.com>
2024-05-15 17:40:49 -0700

    operadores added

--- JavaScript/JS basics.js
+++ JavaScript/JS basics.js
@@ -1,4 +1,4 @@
-// --- variables --- //
+// -------------------------- variables -------------------------- //
 
 let uno;        // "let" tu puedes cambiar el valor y no hace falta INICIALIZAR
 const dos = 1;  // "const" NO puedes cambiar el valor y AHUEVO la tienes que INICIALIZAR
@@ -6,7 +6,8 @@ const dos = 1;  // "const" NO puedes cambiar el valor y AHUEVO la tienes que INI
 
 
 
-// --- booleanos --- //
+
+// -------------------------- booleanos -------------------------- //
 
 let onSale = true;  // verdadero
 let onSalee = false; // falso
@@ -34,3 +35,35 @@ console.log(Boolean(100)) // = true
     // estos son todos lo valores truthy (true)
 
         // todo lo que no estaaba en la lista falsy es truthy
+
+
+
+// -------------------------- operadores -------------------------- //
+
+// devuelve un valor booleano
+
+==      // igualdad (no importa el tipo de variable)
+===     // igualdad triple (si importa el tipo de variable)
+
+!=      // no igualdad (no importa el tipo de variable)
+!==     // no igualdad doble (si importa el tipo de variable)
+
+>       // mayor q
+=>      // mayor o igual q
+
+<       // menor q
+=<      // menor o igual q
+
+&&      // and (los dos tienen q ser true para que devuelva TRUE)
+
+||      // or (minimo UNO tiene que ser true para que devuelva TRUE)
+
+
++       // suma
+-       // resta
+*       // multiplicacion
+/       // division
+**      // potencia
+%       // residuo
+
+
