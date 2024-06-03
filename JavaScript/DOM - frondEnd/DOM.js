76118a15e01b9dcfe62a04756b113c99b0a6b188
bofi1 <angelborque03github@gmail.com>
2024-06-02 22:47:38 -0700

    DOM.js added curso de DOM

---
+++ JavaScript/DOM - frondEnd/DOM.js
@@ -0,0 +1,14 @@
+/* TODO EL CURSO ESTA AQUI https://www.w3schools.com/js/default.asp
+    
+    EJERCICIOS PARA PRACTICAR https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_variables1
+
+document = es el documento en si como si fuera el html
+https://www.freecodecamp.org/news/content/images/2021/09/Document.jpg
+
+*/
+
+document.getElementById() // tomas el elemento con el respectivo id
+Date() // regresa la fecha actual
+// ej. https://www.w3schools.com/js/tryit.asp?filename=tryjs_myfirst
+
+
