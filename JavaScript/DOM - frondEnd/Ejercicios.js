a025d5994676458de9243e9c16b39b4200a3ffe0
bofi1 <angelborque03github@gmail.com>
2024-06-02 22:51:42 -0700

    Ejercicios.js added

---
+++ JavaScript/DOM - frondEnd/Ejercicios.js
@@ -0,0 +1,7 @@
+
+// EJERCICIOS PARA PRACTICAR https://www.w3schools.com/js/exercise_js.asp?filename=exercise_js_variables1
+
+
++= // es mejor que poner x = x + y  MEJOR x += y   tambien se puede hacer con los demas operadores
+
+on // eventos de q se ejecutan cuando hacer algo con el mouse    ej. onclick, onmouseover, etc 
